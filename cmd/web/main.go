e6c790dd92b961eddf67e585f0eb3a99b40c122c
CollCaz <62307305+CollCaz@users.noreply.github.com>
2023-11-05 17:05:28 +0200

    now using command line argument for the HTTP address.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// The HTTP Address
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
@@ -16,7 +22,8 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	fmt.Printf("http://www.localhost%s\n", *addr)
+	log.Printf("Starting server on %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
