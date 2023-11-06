2ccf43569e8724a12583089f2e9b049dd32f3e12
CollCaz <62307305+CollCaz@users.noreply.github.com>
2023-11-06 12:41:27 +0200

    set up sql connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -17,10 +19,20 @@ type application struct {
 func main() {
 	// The HTTP Address
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	// MySQL DSN String
+	dsn := flag.String("dsn", "username:password@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errLog,
@@ -35,6 +47,20 @@ func main() {
 
 	fmt.Printf("http://www.localhost%s\n", *addr)
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srvr.ListenAndServe()
+	err = srvr.ListenAndServe()
 	errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the database and check for errors
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
